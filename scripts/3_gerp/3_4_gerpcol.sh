eb3775ef8d6eaa85c818610946597e787b6959ec
winklerb90-dev <winklerb90@gmail.com>
2026-08-12 14:10:02 +0200

    Update 3_4_gerpcol.sh
    small edit

--- scripts/3_gerp/3_4_gerpcol.sh
+++ scripts/3_gerp/3_4_gerpcol.sh
@@ -40,7 +40,7 @@
 #=========[ + + + + Job Steps + + + + ]==========#
 module load lang/Anaconda3/2024.06-1
 source activate base
-conda activate gerp_env
+conda activate >>insert_conda_env_name< # make sure packages are compiled before
 
 cd /lustre/project/account_name/data/gerp_rates
 
