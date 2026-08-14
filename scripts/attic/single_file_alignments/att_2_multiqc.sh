913b1c99e12238b7e1048340323abffba7ceb8cc
winklerb90-dev <winklerb90@gmail.com>
2026-08-14 12:22:44 +0200

    Create att_2_multiqc.sh
    add multiqc attic script

---
+++ scripts/attic/single_file_alignments/att_2_multiqc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -A >>insert_account_name<<
+#SBATCH -J multiqc
+#SBATCH -p parallel
+#SBATCH -n 1
+#SBATCH -c 40
+#SBATCH -C broadwell
+#SBATCH -t 02:00:00
+#SBATCH -v
+#SBATCH -o output_multiqc.%j.out
+#SBATCH -e error_multiqc.%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=>>insert_email<<
+
+# Load modules:
+module load bio/MultiQC/1.7-foss-2018a-Python-3.6.4
+
+# Manual/ Reference:
+# https://docs.seqera.io/multiqc/
+
+# Description:
+# Summarises and combines the single files QC reports from FastQC into one report
+
+# Run task:
+multiqc ~/projects/path/data/fastqc_reports/filtered/
+
+mv ~/projects/path/scripts/multiqc_data/ ~/projects/path/data/fastqc_reports/filtered/multiqc/
+mv ~/projects/path/scripts/multiqc_report.html ~/projects/path/data/fastqc_reports/filtered/multiqc/
