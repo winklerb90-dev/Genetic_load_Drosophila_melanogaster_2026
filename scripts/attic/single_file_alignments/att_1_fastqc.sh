88540dfcb90d50694ee54765d4985137fdafabd0
winklerb90-dev <winklerb90@gmail.com>
2026-08-14 12:20:08 +0200

    Create att_1_fastqc.sh
    add fastqc attic script

---
+++ scripts/attic/single_file_alignments/att_1_fastqc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH -A >>insert_account_name<<
+#SBATCH -J fastqc_batch1_paired
+#SBATCH -p parallel
+#SBATCH -n 1
+#SBATCH -c 40
+#SBATCH -C broadwell
+#SBATCH -t 08:00:00
+#SBATCH -v
+#SBATCH -o output_fastqc_batch1_paired.%j.out
+#SBATCH -e error_fastqc_batch1_paired.%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=>>insert_email<<
+
+# Load modules:
+module load bio/FastQC/0.11.9-Java-11
+
+# Manual/ Reference:
+# https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
+
+# Desc:
+# Quality control of single fastq files; necessary step for summarised QC report;
+# Run in batches of hundred to speed up the process
+
+# Run task:
+for i in ~/projects/path/data/fastq_files/filtered/paired/batch1/*
+do
+	fastqc i \
+	-o ~/projects/m2_jgu-salmosex/drosophila_selection_project_2026/data/fastqc_reports/filtered $i
+done
+
